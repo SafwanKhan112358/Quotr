a8f80e2c7c47aadd7ebdb617fe230b72d9143526
SafwanKhan112358 <safwan.khan0305@gmail.com>
2023-07-19 02:28:59 -0400

    Working towards displaying an overlay over a user's active browser window. Attempted to execute this feature, by injecting a content script into active browser window. The popup.js file communicates with the content script about when to inject the script

---
+++ contentScript.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+
+});
+
+
--- popup.js
+++ popup.js
@@ -4,7 +4,6 @@ import { countKey } from './constants.js';
 import { resultsKey } from './constants.js';
 
 
-
 function getEmotion(event) {
   const clickedEmotionButton = event.target;
   const emotion = clickedEmotionButton.id;
@@ -30,14 +29,18 @@ async function getQuote(event) {
   const response = await getApiResponse(baseApiUrl, event);
   const quoteKeyNumber = Math.floor(Math.random() * (response[countKey]));
 
-  console.log(response[resultsKey][quoteKeyNumber][contentKey]);
+  const quote = response[resultsKey][quoteKeyNumber][contentKey];
+  // Close the popup
+  //window.close();
 
-
+  // Send a message to the content script to create the overlay
+  setTimeout(function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log(tabs);
+      chrome.tabs.sendMessage(tabs[0].id, { message: quote });
+    });
+  }, 2000); // Add a delay of 1 second (adjust as needed)
 }
-
-
-
-
 
 //Worth Putting more Attention here
 //TODO: clean this code, need to add more emotions
